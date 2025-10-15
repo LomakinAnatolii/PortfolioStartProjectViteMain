ba5c2722b4b936440160b993c74cd391793df3d6
LomakinAnatoliy <lomakin90anatoliy@gmail.com>
2025-10-15 19:41:08 +0200

    feat: layout the header sections
    This reverts commit 89d8790d

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Icon} from "../icons/Icon.tsx";
+
+export const Logo = () => {
+    return (
+        <a href="">
+            <Icon iconId={'codeSvg'}/>
+        </a>
+    );
+};
