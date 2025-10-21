b0b4e3ac3c2dc3514e73a11aa6b7d01fa958ea3a
LomakinAnatoliy <lomakin90anatoliy@gmail.com>
2025-10-21 20:33:46 +0200

    style: add styles for the slogan block

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,15 +1,20 @@
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle.tsx";
+import {Button} from "../../../components/Button.tsx";
+
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
-            <SectionTitle text={"I am available for Freelance"}/>
-            <button type={"button"}>Hire me</button>
+                <SectionTitle text={"I am available for Freelance"}/>
+                <Button type={"button"}>Hire me</Button>
         </StyledSlogan>
     );
 };
 
 const StyledSlogan = styled.section`
-
+    padding: 100px 30px;
+    ${Button} {
+        margin: 0 auto;
+    }
 `
